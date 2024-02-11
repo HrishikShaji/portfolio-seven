a830d824d0fe26a8ab44a3ba122012b75dfd19f7
HrishikShaji <hrishikminishaji@gmail.com>
2024-02-11 23:00:43 +0530

    added testimonial animation

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -3,38 +3,144 @@ import { useGSAP } from "@gsap/react";
 import { data } from "../lib/data";
 import { useRef } from "react";
 import gsap from "gsap";
+import Link from "next/link";
 
 export const Projects = () => {
-  const projectsRef = useRef<HTMLDivElement>(null);
-  const targetRef = useRef<HTMLDivElement>(null);
+	const projectsRef = useRef<HTMLDivElement>(null);
+	const targetRef = useRef<HTMLDivElement>(null);
 
-  useGSAP(() => {
-    if (projectsRef.current && targetRef.current) {
-      gsap.to(targetRef.current, {
-        scrollTrigger: {
-          trigger: projectsRef.current,
-          pin: targetRef.current,
-          start: "top 20%",
-          end: "bottom 80%",
-          scrub: true,
-        },
-      });
-    }
-  }, {});
-  return (
-    <div className="  text-black py-40  flex justify-center w-full items-center ">
-      <div className="flex w-[60vw]">
-        <div ref={targetRef} className="w-[50%] h-full ">
-          <h1 className="text-6xl text-black">Projects</h1>
-        </div>
-        <div ref={projectsRef} className="w-[50%] h-full flex flex-col">
-          {data.projects.data.map((item, i) => (
-            <div className="py-5" key={i}>
-              <h1 className="text-3xl">{item.title}</h1>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
+	useGSAP(() => {
+		if (projectsRef.current && targetRef.current) {
+			gsap.to(targetRef.current, {
+				scrollTrigger: {
+					trigger: projectsRef.current,
+					pin: targetRef.current,
+					start: "top 20%",
+					end: "bottom 80%",
+					scrub: true,
+				},
+			});
+		}
+	}, {});
+	return (
+		<div className="  text-black py-40  flex justify-center w-full items-center ">
+			<div className="flex w-[60vw]">
+				<div ref={targetRef} className="w-[50%] h-full ">
+					<h1 className="text-6xl text-black">Projects</h1>
+				</div>
+				<div ref={projectsRef} className="w-[50%] h-full flex flex-col">
+					{data.projects.data.map((item, i) => (
+						<Project key={i} item={item} />
+					))}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+interface ProjectProps {
+	item: any;
+}
+
+const Project: React.FC<ProjectProps> = ({ item }) => {
+	const arrowRef = useRef<HTMLDivElement>(null);
+	const itemRef = useRef<HTMLDivElement>(null);
+	const titleRef = useRef<HTMLHeadingElement>(null);
+	const linkRef = useRef<HTMLAnchorElement>(null);
+	useGSAP(() => {
+		if (
+			titleRef.current &&
+			arrowRef.current &&
+			itemRef.current &&
+			linkRef.current
+		) {
+			const tl = gsap.timeline({
+				scrollTrigger: {
+					trigger: itemRef.current,
+					start: "top 35%",
+					end: "top 5%",
+					scrub: true,
+				},
+			});
+
+			const arrowTl = gsap.timeline({
+				scrollTrigger: {
+					trigger: arrowRef.current,
+					start: "top 35%",
+					end: "top 5%",
+					scrub: true,
+				},
+			});
+
+			const linkTl = gsap.timeline({
+				scrollTrigger: {
+					trigger: arrowRef.current,
+					start: "top 25%",
+					end: "top 15%",
+					scrub: true,
+				},
+			});
+
+			tl.fromTo(
+				titleRef.current,
+
+				{
+					scale: 1,
+				},
+				{
+					transformOrigin: "left",
+					scale: 2,
+				},
+			).to(titleRef.current, {
+				scale: 1,
+			});
+
+			arrowTl
+				.fromTo(
+					arrowRef.current,
+					{
+						scale: 0,
+					},
+					{
+						scale: 1,
+						transformOrigin: "center",
+					},
+				)
+				.to(arrowRef.current, {
+					scale: 0,
+				});
+
+			linkTl
+				.fromTo(
+					linkRef.current,
+					{
+						opacity: 0,
+					},
+					{
+						opacity: 1,
+					},
+				)
+				.to(linkRef.current, {
+					opacity: 0,
+				});
+		}
+	}, {});
+	return (
+		<div
+			ref={itemRef}
+			className="py-10 flex relative justify-between items-center gap-2"
+		>
+			<h1 ref={titleRef} className="text-3xl">
+				{item.title}
+			</h1>
+			<div
+				ref={arrowRef}
+				className="h-24 w-24 rounded-full flex justify-center items-center absolute -right-40 bg-black"
+			>
+				<Link href="/" ref={linkRef} className="text-white">
+					o
+				</Link>
+			</div>
+		</div>
+	);
 };
--- app/components/Testimonials.tsx
+++ app/components/Testimonials.tsx
@@ -3,6 +3,7 @@ import { useGSAP } from "@gsap/react";
 import { data } from "../lib/data";
 import { useRef } from "react";
 import gsap from "gsap";
+import Image from "next/image";
 
 export const Testimonials = () => {
 	const testimonialsRef = useRef<HTMLDivElement>(null);
@@ -29,13 +30,91 @@ export const Testimonials = () => {
 				</div>
 				<div ref={testimonialsRef} className="w-[50%] h-full flex flex-col">
 					{data.testimonials.map((item, i) => (
-						<div className="py-5 flex flex-col gap-2" key={i}>
-							<h1 className="text-3xl">{item.name}</h1>
-							<h1>{item.desc}</h1>
-						</div>
+						<Testimonial key={i} item={item} />
 					))}
 				</div>
 			</div>
+		</div>
+	);
+};
+
+interface TestimonialProps {
+	item: any;
+}
+
+const Testimonial: React.FC<TestimonialProps> = ({ item }) => {
+	const circleRef = useRef<HTMLImageElement>(null);
+	const itemRef = useRef<HTMLDivElement>(null);
+	const titleRef = useRef<HTMLHeadingElement>(null);
+	useGSAP(() => {
+		if (titleRef.current && circleRef.current && itemRef.current) {
+			const tl = gsap.timeline({
+				scrollTrigger: {
+					trigger: itemRef.current,
+					start: "top 35%",
+					end: "top 5%",
+					scrub: true,
+				},
+			});
+
+			const circleTl = gsap.timeline({
+				scrollTrigger: {
+					trigger: circleRef.current,
+					start: "top 35%",
+					end: "top 5%",
+					scrub: true,
+				},
+			});
+
+			tl.fromTo(
+				titleRef.current,
+
+				{
+					scale: 1,
+				},
+				{
+					transformOrigin: "left",
+					scale: 2,
+				},
+			).to(titleRef.current, {
+				scale: 1,
+			});
+
+			circleTl
+				.fromTo(
+					circleRef.current,
+					{
+						scale: 0,
+					},
+					{
+						scale: 1,
+						transformOrigin: "center",
+					},
+				)
+				.to(circleRef.current, {
+					scale: 0,
+				});
+		}
+	}, {});
+	return (
+		<div
+			ref={itemRef}
+			className="py-10 flex relative justify-between items-start gap-2"
+		>
+			<div className="py-5 flex flex-col gap-10">
+				<h1 className="text-3xl" ref={titleRef}>
+					{item.name}
+				</h1>
+				<h1>{item.desc}</h1>
+			</div>
+			<Image
+				src={item.img}
+				height={1000}
+				width={1000}
+				alt="image"
+				ref={circleRef}
+				className="h-24 object-cover w-24 rounded-full absolute -right-40 bg-black"
+			/>
 		</div>
 	);
 };
